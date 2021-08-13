7997d093367d0b8990297b9cef31a30d6029142b
swagatika1224 <81203604+swagatika1224@users.noreply.github.com>
2021-08-12 17:36:37 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-//https://teachablemachine.withgoogle.com/models/PG1eZML88/
+//https://teachablemachine.withgoogle.com/models/-ubKWHroe/
+Webcam.set({
+width:350,
+height:300,
+image_format : 'png',
+png_quality:90
+});
+
+camera = document.getElementById("camera");
+Webcam.attach('#camera');
+
+function take_snapshot() {
+Webcam.snap(function(data_uri) {
+document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'"/>'
+    });
+}
+
+console.log('ml5 version:', ml5.version);
