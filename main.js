5a42c1260ff348800e9c1f6a58310c4335210b16
swagatika1224 <81203604+swagatika1224@users.noreply.github.com>
2021-08-12 18:06:24 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,18 +1,19 @@
-//https://teachablemachine.withgoogle.com/models/-ubKWHroe/
+//https://teachablemachine.withgoogle.com/models/PG1eZML88/
+
 Webcam.set({
-width:350,
-height:300,
-image_format : 'png',
-png_quality:90
-});
-
-camera = document.getElementById("camera");
-Webcam.attach('#camera');
-
-function take_snapshot() {
-Webcam.snap(function(data_uri) {
-document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'"/>'
+    width:350,
+    height:300,
+    image_format : 'png',
+    png_quality:90
     });
-}
-
-console.log('ml5 version:', ml5.version);
+    
+    camera = document.getElementById("camera");
+    Webcam.attach('#camera');
+    
+    function take_snapshot() {
+    Webcam.snap(function(data_uri) {
+    document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'"/>'
+        });
+    }
+    
+    console.log('ml5 version:', ml5.version);
