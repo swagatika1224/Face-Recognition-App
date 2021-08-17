ebe8d9606feb3f35c351237d9a062013500779bc
swagatika1224 <81203604+swagatika1224@users.noreply.github.com>
2021-08-16 19:13:35 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -17,3 +17,24 @@ Webcam.set({
     }
     
     console.log('ml5 version:', ml5.version);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/3V43t1dGR/v_sl95BzE/model.json',modelLoaded);
+
+function modelLoaded() {
+console.log('Model Loaded!');
+}
+
+function check() {
+img = document.getElementById('captured_image');
+classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results) {
+if(error) {
+console.error(error);
+} else {
+console.log(results);
+document.getElementById("result_object_name").innerHTML = results[0].label;
+document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
+ }
+
