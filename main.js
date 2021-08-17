fb8772945a1585aeebdd2e7d4a3b12306df12947
swagatika1224 <81203604+swagatika1224@users.noreply.github.com>
2021-08-16 18:51:58 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,38 +1,19 @@
+//https://teachablemachine.withgoogle.com/models/PG1eZML88/
+
 Webcam.set({
-width:350,
-height:300,
-image_format : 'png',
-png_quality:90
-});
-
-camera = document.getElementById("camera");
-Webcam.attach('#camera');
-
-function take_snapshot() {
-Webcam.snap(function(data_uri) {
-document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'"/>'
+    width:350,
+    height:300,
+    image_format : 'png',
+    png_quality:90
     });
-}
-
-console.log('ml5 version:', ml5.version);
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/v_sl95BzE/model.json/models/v_sl95BzE/model.json',modelLoaded);
-
-function modelLoaded() {
-console.log('Model Loaded!');
-}
-
-function check() {
-img = document.getElementById('captured_image');
-classifier.classify(img, gotResult);
-}
-
-function gotResult(error, results) {
-if(error) {
-console.error(error);
-} else {
-console.log(results);
-document.getElementById("result_object_name").innerHTML = results[0].label;
-document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+    
+    camera = document.getElementById("camera");
+    Webcam.attach('#camera');
+    
+    function take_snapshot() {
+    Webcam.snap(function(data_uri) {
+    document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'"/>'
+        });
     }
- }
-
+    
+    console.log('ml5 version:', ml5.version);
